bb7fcff2e725cb23bae05e368d8828bff1588e8b
OrekiPotato <mojangthesimlord@outlook.com>
2024-01-24 14:02:12 +0800

    Attempted Qns2 again, Lobby UI shows user created room

---
+++ PGGE_A2/Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    public InputField roomInputField;
+    public GameObject lobbyPanel;
+
+    public RoomItem roomItemPrefab;
+    List<RoomItem> roomItemsList = new List<RoomItem>();
+    public Transform contentObject;
+
+    public float updateDelay = 1.5f;
+    float nextUpdateTime;
+
+    bool connected = false;
+
+    private void Start()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    //public override void OnJoinedLobby()
+    //{
+    //    if (connected)
+    //    {
+    //        StartCoroutine(CreateDumbRoomsAfterDelay());
+    //    }
+    //}
+
+    //IEnumerator CreateDumbRoomsAfterDelay()
+    //{
+    //    yield return new WaitForSeconds(1.5f);
+    //    StartCoroutine(CreateDumbRooms());
+    //}
+
+    //IEnumerator CreateDumbRooms()
+    //{
+    //    yield return new WaitForSeconds(1.0f);
+
+    //    // Creates the 5 dummy rooms
+    //    for (int i = 1; i <= 5; i++)
+    //    {
+    //        string dumbRoomName = "Dumb Room" + i;
+    //        RoomOptions roomOptions = new RoomOptions() { MaxPlayers = 3 };
+    //        PhotonNetwork.CreateRoom(dumbRoomName, roomOptions, TypedLobby.Default);
+
+    //        // Instantiates the new dummy rooms into room list.
+    //        RoomItem newDumbRoom = Instantiate(roomItemPrefab, contentObject);
+    //        newDumbRoom.SetRoomName(dumbRoomName);
+    //        roomItemsList.Add(newDumbRoom);
+    //    }
+    //}
+
+
+
+    public void OnClickCreate()
+    {
+        if (roomInputField.text.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() { MaxPlayers = 3 });
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        lobbyPanel.SetActive(false);
+        PhotonNetwork.LoadLevel("MultiplayerMap00");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        Debug.Log("Room list updated. Count: " + roomList.Count);
+        UpdateRoomList(roomList);
+    }
+
+    void UpdateRoomList(List<RoomInfo> list)
+    {
+        // Clears the list of any items
+        foreach (RoomItem item in roomItemsList)
+        {
+            Destroy(item.gameObject);
+        }
+        roomItemsList.Clear();
+
+
+        // Populates the list of rooms in the Lobby
+        foreach (RoomInfo room in list)
+        {
+            RoomItem newRoom = Instantiate(roomItemPrefab, contentObject);
+            newRoom.SetRoomName(room.Name);
+            roomItemsList.Add(newRoom);
+        }
+
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+}
